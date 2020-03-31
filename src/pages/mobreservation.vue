cab57e204e9fe87e5d799b758a6484f9be70892c
raph <paengui17@gmail.com>
2020-03-31 21:16:39 +0800

    reservation status layout update.

--- src/pages/mobreservation.vue
+++ src/pages/mobreservation.vue
@@ -1,98 +1,27 @@
 <template>
-    <q-page class="q-pt-md">
-        <q-list separator>
-            <q-item>
-                <q-item-section>
-                    <div class="text-center">    
-                        <q-chip color="deep-orange-4" text-color="white">
-                            Event Name
-                        </q-chip>
-                    </div>
-                    <div class="column q-gutter-sm">
-                        <div class="text-weight-bold">Name of Customer:</div>
-                        <div class="text-weight-bold">Date of Event:</div>
-                        <div class="row">
-                            <div class="col text-weight-bold">Time Start:</div> 
-                            <div class="text-weight-bold q-pr-xl">Time End: </div>
-                        </div>
-                        <div class="text-weight-bold">Place of Event:</div>
-                        <div class="text-weight-bold">Guest:</div>
-                        <div class="text-weight-bold">Email Address:</div>
-                        <div class="text-weight-bold">Balance Payment:</div>    
-                    </div>
-                    <div class="row justify-between q-pt-md">
-                        <div>
-                            <q-btn dense style="width:150px" outline color="deep-orange-4" label="View Details" @click="details = true" />
-                        </div>
-                        <div>
-                            <q-btn dense style="width:150px" outline color="deep-orange-4" label="Payment" @click="payment = true" />
-                        </div>
-                    </div>
-                </q-item-section>
-            </q-item>
-
-            <q-item>
-                <q-item-section>
-                    <div class="text-center">    
-                        <q-chip color="deep-orange-4" text-color="white">
-                            Event Name
-                        </q-chip>
-                    </div>
-                    <div class="column q-gutter-sm">
-                        <div class="text-weight-bold">Name of Customer:</div>
-                        <div class="text-weight-bold">Date of Event:</div>
-                        <div class="row">
-                            <div class="col text-weight-bold">Time Start:</div> 
-                            <div class="text-weight-bold q-pr-xl">Time End: </div>
-                        </div>
-                        <div class="text-weight-bold">Place of Event:</div>
-                        <div class="text-weight-bold">Guest:</div>
-                        <div class="text-weight-bold">Email Address:</div>
-                        <div class="text-weight-bold">Balance Payment:</div>    
-                    </div>
-                    <div class="row justify-between q-pt-md">
-                        <div>
-                            <q-btn dense style="width:150px" outline color="deep-orange-4" label="View Details" @click="details = true" />
-                        </div>
-                        <div>
-                            <q-btn dense style="width:150px" outline color="deep-orange-4" label="Payment" @click="payment = true" />
-                        </div>
-                    </div>
-                </q-item-section>
-            </q-item>
-        </q-list>
-
-<!-- PAYMENT DIALOG -->
-        <q-dialog v-model="payment" persistent>
-            <q-card style="min-width: 350px">
-                <q-card-section>
-                <div class="text-h6">Payment</div>
-                </q-card-section>
-
-                <q-card-section class="q-pt-none">
-                <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" />
-                </q-card-section>
-
-                <q-card-actions align="right" class="text-primary">
-                <q-btn flat label="Cancel" v-close-popup />
-                <q-btn flat label="Add address" v-close-popup />
-                </q-card-actions>
-            </q-card>
-        </q-dialog>
-<!-- END OF PAYMENT -->
-
-<!-- PAYMENT DETAILS -->
-        <q-dialog v-model="details" persistent>
-            <q-card style="min-width: 350px">
-                <q-card-section>
-                <div class="text-h6">Other Details</div>
-
-                <div class="q-pt-lg text-weight-bold">
-                    Motif: 
-                </div>
-                <div class="q-pt-sm text-weight-bold">
-                    Package Name: 
-                </div>
+    <q-page class="q-pt-md q-pa-md">
+        <div class="text-right">
+            <q-btn dense style="width:100px" outline color="deep-orange-4" label="Payment" @click="payment = true" />
+        </div>
+        <div class="text-center q-pt-sm">    
+            <q-chip color="deep-orange-4" size="xl" text-color="white">
+                Event Name
+            </q-chip>
+        </div>
+        <div class="column q-pt-sm q-gutter-sm">
+            <div class="text-weight-bold">Name of Customer:</div>
+            <div class="text-weight-bold">Date of Event:</div>
+            <div class="row q-pr-lg">
+                <div class="col text-weight-bold">Time Start:</div> 
+                <div class="text-weight-bold q-pr-xl">Time End: </div>
+            </div>
+            <div class="text-weight-bold">Place of Event:</div>
+            <div class="text-weight-bold">Guest:</div>
+            <div class="text-weight-bold">Email Address:</div>
+            <div class="text-weight-bold">Balance Payment:</div>    
+            <div class="text-weight-bold">Motif: </div>
+            <div class="text-weight-bold">Package Name: </div>
+        </div>
                 <div class="q-pt-sm text-weight-bold">
                     Foods: 
                 </div>
@@ -204,14 +133,25 @@
                     </q-item>
                  </q-list>
 
+
+<!-- PAYMENT DIALOG -->
+        <q-dialog v-model="payment" persistent>
+            <q-card style="min-width: 350px">
+                <q-card-section>
+                <div class="text-h6">Payment</div>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" />
                 </q-card-section>
 
                 <q-card-actions align="right" class="text-primary">
                 <q-btn flat label="Cancel" v-close-popup />
+                <q-btn flat label="Add address" v-close-popup />
                 </q-card-actions>
             </q-card>
         </q-dialog>
-<!-- END OF DETAILS -->
+<!-- END OF PAYMENT -->
     </q-page>
 </template>
 
@@ -220,7 +160,6 @@ export default {
   data () {
     return {
       payment: false,
-      details: false,
     }
   }
 }
